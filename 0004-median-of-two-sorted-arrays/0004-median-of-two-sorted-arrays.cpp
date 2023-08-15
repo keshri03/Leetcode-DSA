3940a34e308b38f39a8f96aed8cb02cbc9730d80
Raja Keshri <79037052+keshri03@users.noreply.github.com>
2023-08-15 15:01:02 +0530

    Time: 16 ms (97.67%), Space: 89.3 MB (55.78%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,82 +1,53 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int i=0,j=0;
-        int tot=nums1.size()+nums2.size();
-        int count=0;
-        double ans=0;
-        while(i<=nums1.size() || j<=nums2.size()){
-
-            if(i<nums1.size() && j<nums2.size()){
-                if(nums1[i]<=nums2[j]){
-                    count++;
-                    if(count==(tot/2)){
-                        if(tot%2==0){
-                            ans+=nums1[i];
-                        }
-                    }
-                    if(count==(tot/2)+1){
-                        if(tot%2==0){
-                            ans=(ans+nums1[i])*1.0/2;
-                        }
-                        else ans+=nums1[i];
-                        break;
-                    }
-                    i++;
-                }
-                else{
-                    count++;
-                    if(count==(tot/2)){
-                        if(tot%2==0){
-                            ans+=nums2[j];
-                        }
-                    }
-                    if(count==(tot/2)+1){
-                        if(tot%2==0){
-                            ans=(ans+nums2[j])*1.0/2;
-                        }
-                        else ans+=nums2[j];
-                        break;
-                    }
-                    j++;
-                }
-            }
-            else if(i<nums1.size()){
-                count++;
-                    if(count==(tot/2)){
-                        if(tot%2==0){
-                            ans+=nums1[i];
-                        }
-                    }
-                    if(count==(tot/2)+1){
-                        if(tot%2==0){
-                            ans=(ans+nums1[i])*1.0/2;
-                        }
-                        else ans+=nums1[i];
-                        break;
-                    }
-                    i++;
-
-            }
-            else{
-                count++;
-                    if(count==(tot/2)){
-                        if(tot%2==0){
-                            ans+=nums2[j];
-                        }
-                    }
-                    if(count==(tot/2)+1){
-                        if(tot%2==0){
-                            ans=(ans+nums2[j])*1.0/2;
-                        }
-                        else ans+=nums2[j];
-                        break;
-                    }
-                    j++;
-
-            }
-        }
-        return ans;
+    double median(vector<int>&nums1,vector<int>&nums2){
+        int n=nums1.size();
+        int m=nums2.size();
+        int req=(m+n)/2;
+        if((m+n)%2==1) req++;
+        int l=0,h=n;
+        while(l<=h){
         
+        int mid=(l+h)/2;
+        // cout<<l<<" "<<h<<" "<<mid<<" "<<req<<endl;
+        int l1,l2,r1,r2;
+        int rem=(req-mid);
+        if(mid==0) l1=INT_MIN;
+        else l1=nums1[mid-1];
+        if(rem==0) l2=INT_MIN;
+        else l2=nums2[rem-1];
+        if(mid+1>n) r1=INT_MAX;
+        else r1=nums1[mid];
+        if(rem+1>m) r2=INT_MAX;
+        else r2=nums2[rem];
+        // cout<<l1<<" "<<l2<<" "<<r1<<" "<<r2<<endl;
+        if(l1>r2){
+            h=mid-1;
+        }
+        else if(l2>r1){
+            l=mid+1;
+        }
+        else{
+            if((m+n)%2==1){
+                return max(l1,l2);
+            }
+            else return 1.0*((max(l1,l2)+min(r1,r2)))/2;
+        }                   
+        }
+        return -1;
+
+    }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        int m=nums2.size();
+        
+        if(n<=m){
+            return median(nums1,nums2);
+            
+        }
+        else{
+            return median(nums2,nums1);
+        }
+    
     }
 };
